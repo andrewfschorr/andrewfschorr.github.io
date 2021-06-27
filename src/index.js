811563db811481b214c78d7ae8cbd05cc71acd4d
Andrew Schorr <afs35mm@gmail.com>
2021-06-27 11:14:28 -0400

    Redid favicon and fixed linkedin link

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ ctx.lineWidth = 2;
 ctx.strokeStyle = 'red';
 
 const LINES = 12;
-const TOTAL_POINTS = (LINES + 1) * (LINES + 1);
-const DEGREES = (TOTAL_POINTS / 360);
-const LENGTH = window.innerWidth / (LINES + 1);
 
+
+const LENGTH = window.innerWidth / (LINES + 1);
+const VERTICAL_LINES = Math.floor(window.innerHeight / LENGTH);
+const TOTAL_POINTS = LENGTH * VERTICAL_LINES;
+const DEGREES = (TOTAL_POINTS / 720); // i have no idea why this is 720, just looks cooler
 const SECONDS_PER_ROTATION = 12;
 const FPS = 60; // have to just assume 60 fsp
 const UNIT_PER_FRAME = (360 / FPS) / SECONDS_PER_ROTATION;
@@ -44,18 +46,15 @@ function init() {
 function draw() {
   j += UNIT_PER_FRAME;
 
-  let i = 0 + j;
-  const verticalLines = Math.floor(window.innerHeight / LENGTH);
-
+  let i = 0;
   ctx.clearRect(0, 0, c.width, c.height);
   ctx.fillStyle = rgb(255, 255, 255);
   ctx.fillRect(0, 0, c.width, c.height);
 
-
-  for (let r = 0; r <= verticalLines + 1; r++) {
+  for (let r = 0; r <= VERTICAL_LINES + 1; r++) {
     for (let c = 0; c <= LINES + 1; c++) {
       i++;
-      drawLine(r * LENGTH, c * LENGTH, LENGTH, i * DEGREES)
+      drawLine(r * LENGTH, c * LENGTH, LENGTH, i * DEGREES + j);
     }
   }
   window.requestAnimationFrame(draw);
